711cb908bed89e8e605e5e43e3cdd4fc7a06bdf4
sharpAll <380232725@qq.com>
2019-03-08 13:23:08 +0800

    fresh

--- js/QD.js
+++ js/QD.js
@@ -153,7 +153,7 @@ function mapInit() {
     var borderWidth=remVal/6;
     var myChart = echarts.init(document.getElementById('center-part'));
     var option = {
-        color: ['#f4d125','#1aad08'],
+        color: ['#f4d125','#1aad08','#72bed0'],
         backgroundColor: 'transparent',
         series: [
             {
@@ -162,11 +162,16 @@ function mapInit() {
                 data:[],
                 geoCoord: {
                     '城阳区': [120.37,36.30],
-                    '黄岛区':[120.18,35.97],
-                    '即墨市':[120.45,36.38],
-                    '胶南市':[120.03,35.87],
+                    '西海岸经济新区':[120.18,35.97],
+                    '即墨区':[120.45,36.38],
                     '胶州市':[120.03,36.27],
                     '莱西市':[120.50,36.87],
+                    '李沧区':[120.43 ,36.15],
+                    '平度市':[119.95,36.78],
+                    '市北区':[120.38 ,36.08],
+                    '市南区':[120.38,36.07],
+                    '四方区':[120.35 ,36.10],
+                    '崂山区':[120.47 ,36.10],
                     '奥帆中心':[120.397399,36.06077],
                     '媒体中心':[120.448266,36.076505],
                     '飞机场':[120.022632,36.31902],
@@ -185,10 +190,19 @@ function mapInit() {
                         borderWidth:borderWidth,
                         label:{
                             show:true,
+                            formatter:function (o) {
+                                console.log(o);
+                                if(o=='四方区'||o=='胶南市'){
+                                    return ' ';
+                                }else{
+                                    return o
+                                }
+                            },
                             textStyle:{
                                 color:'#fff',
                                 fontSize:labelSize
                             }
+
                         }
                     },
                     emphasis: {
@@ -222,8 +236,8 @@ function mapInit() {
                         }
                     },
                     data : [
-                        {name: '奥帆中心', value: 100},
-                        {name: '媒体中心', value: 50},
+                        // {name: '奥帆中心', value: 100},
+                        // {name: '媒体中心', value: 50},
                         {name: '飞机场', value: 70},
                         {name: '火车站', value: 50}
                     ]
@@ -274,6 +288,78 @@ function mapInit() {
                         {name: '海关总署青岛教育培训基地', value: 40}
                     ]
                 }
+            },
+            {
+                name:'四方区',
+                type:'map',
+                mapType: 'QD',
+                data:[],
+                markLine : {
+                    large:true,
+                    symbolSize:[2,2],
+                    smooth:true,
+                    effect : {
+                        show: true,
+                        scaleSize: 1,
+                        period: 30,
+                        color: '#fff',
+                        shadowBlur: 10
+                    },
+                    itemStyle : {
+                        normal: {
+                            label:{
+                                show:false
+                            },
+                            borderWidth:1,
+                            lineStyle: {
+                                type: 'solid',
+                                shadowBlur: 10
+                            }
+                        }
+                    },
+                    data : [
+                        [{name:'城阳区'},{name:'四方区'}],
+                        [{name:'西海岸经济新区'},{name:'四方区'}],
+                        [{name:'即墨区'},{name:'四方区'}],
+                        [{name:'胶州市'},{name:'四方区'}],
+                        [{name:'莱西市'},{name:'四方区'}],
+                        [{name:'李沧区'},{name:'四方区'}],
+                        [{name:'平度市'},{name:'四方区'}],
+                        [{name:'市北区'},{name:'四方区'}],
+                        [{name:'市南区'},{name:'四方区'}],
+                        [{name:'崂山区'},{name:'四方区'}]
+                    ]
+                },
+                markPoint : {
+                    symbol:'emptyCircle',
+                    symbolSize : function (v){
+                        return 10 + v/10
+                    },
+                    effect : {
+                        show: true,
+                        shadowBlur : 0
+                    },
+                    itemStyle:{
+                        normal:{
+                            label:{
+                                show:false
+                            }
+                        }
+                    },
+                    data : [
+                        {name:'城阳区',value:10},
+                        {name:'西海岸经济新区',value:10},
+                        {name:'即墨区',value:10},
+                        {name:'胶州市',value:10},
+                        {name:'莱西市',value:10},
+                        {name:'李沧区',value:10},
+                        {name:'平度市',value:10},
+                        {name:'市北区',value:10},
+                        {name:'市南区',value:10},
+                        {name:'崂山区',value:10}
+                    ]
+                }
+
             }
         ]
     };
